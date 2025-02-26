ef68edd7df6d8ca592003b7ebbc40ecafd6c4cc4
barbarafreitas22 <barbarafreitas2229@gmail.com>
2025-02-26 11:26:43 +0000

    Update Branch_and_Bound.py

--- Motifs/Branch_and_Bound.py
+++ Motifs/Branch_and_Bound.py
@@ -1,4 +1,17 @@
 def procura_exaustiva_motifs(seqs, L):
+    """
+    Searches for the best motifs of length L in the given sequences by exhaustively trying all possible positions, 
+    selecting the combination with the highest conservation score.
+
+    Args:
+        seqs (list): List of biological sequences (strings).
+        L (int): Length of the motif to find.
+
+    Returns:
+        tuple: A tuple containing:
+            - best_s (list): List of start positions for the best motifs in each sequence.
+            - best_score (int): The score of the best motif set.
+    """
     best_score, best_s = 0, None
     tam_seq = len(seqs[0])
     limite = tam_seq - L + 1 
