7782e3e9588a5b2fe242552ddcecb7d3231ceab2
barbarafreitas22 <barbarafreitas2229@gmail.com>
2025-04-08 22:04:18 +0100

    Update test_boyer_moore.py

--- test_boyer_moore.py
+++ test_boyer_moore.py
@@ -1,36 +1,37 @@
 import unittest
 
-class TestBoyerMoore(unittest.TestCase):
-    
-    def test_single_match(self):
-        bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("ATAGAACCAATG")
-        self.assertEqual(result, [6]) 
+class TesteBoyerMoore(unittest.TestCase):
 
-    def test_multiple_matches(self):
+    def test_uma_ocorrencia(self):
         bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("ACCAACCAACCA")
-        self.assertEqual(result, [0, 4, 8])  # Ocorrências em 0, 4 e 8
+        resultado = bm.procurar_padrao("ATAGAACCAATG")
+        self.assertEqual(resultado, [6])  # "ACCA" começa na posição 6
 
-    def test_no_match(self):
+    def test_varias_ocorrencias(self):
         bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("ATAGATGTGTAGTG")
-        self.assertEqual(result, [])  # Nenhuma ocorrência
+        resultado = bm.procurar_padrao("ACCAACCAACCA")
+        self.assertEqual(resultado, [0, 4, 8])
 
-    def test_pattern_longer_than_text(self):
+    def test_nenhuma_ocorrencia(self):
         bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("AC")
-        self.assertEqual(result, [])  # Padrão maior do que o texto
+        resultado = bm.procurar_padrao("ATAGATGTGTAGTG")
+        self.assertEqual(resultado, [])
 
-    def test_empty_text(self):
+    def test_padrao_maior_que_texto(self):
         bm = BoyerMoore("ACTG", "ACCA")
-        result = bm.search_pattern("")
-        self.assertEqual(result, [])  # Texto vazio
+        resultado = bm.procurar_padrao("AC")
+        self.assertEqual(resultado, [])
 
-    def test_empty_pattern(self):
+    def test_texto_vazio(self):
+        bm = BoyerMoore("ACTG", "ACCA")
+        resultado = bm.procurar_padrao("")
+        self.assertEqual(resultado, [])
+
+    def test_padrao_vazio(self):
         bm = BoyerMoore("ACTG", "")
-        result = bm.search_pattern("ACCA")
-        self.assertEqual(result, [])  # Padrão vazio, nenhum resultado
+        resultado = bm.procurar_padrao("ACCA")
+        self.assertEqual(resultado, [])
 
 if __name__ == '__main__':
     unittest.main()
+
