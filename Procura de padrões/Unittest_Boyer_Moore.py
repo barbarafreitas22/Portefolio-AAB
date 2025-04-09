f0a7baa34402d504eba85928ee3983ea6c85b0e0
barbarafreitas22 <barbarafreitas2229@gmail.com>
2025-04-09 11:23:36 +0100

    Update Unittest_Boyer_Moore.py

--- Procura de padrões/Unittest_Boyer_Moore.py
+++ Procura de padrões/Unittest_Boyer_Moore.py
@@ -1,6 +1,5 @@
 import unittest
-
-from BoyerMoore import BoyerMoore
+from BoyerMoore import BoyerMoore  # Certifique-se de que a classe BoyerMoore está no mesmo diretório ou no PYTHONPATH
 
 class TesteBoyerMoore(unittest.TestCase):
 
@@ -12,27 +11,26 @@ class TesteBoyerMoore(unittest.TestCase):
     def test_varias_ocorrencias(self):
         bm = BoyerMoore("ACTG", "ACCA")
         resultado = bm.procurar_padrao("ACCAACCAACCA")
-        self.assertEqual(resultado, [0, 4, 8])
+        self.assertEqual(resultado, [0, 4, 8]) 
 
     def test_nenhuma_ocorrencia(self):
         bm = BoyerMoore("ACTG", "ACCA")
         resultado = bm.procurar_padrao("ATAGATGTGTAGTG")
-        self.assertEqual(resultado, [])
-
+        self.assertEqual(resultado, []) 
     def test_padrao_maior_que_texto(self):
         bm = BoyerMoore("ACTG", "ACCA")
         resultado = bm.procurar_padrao("AC")
-        self.assertEqual(resultado, [])
+        self.assertEqual(resultado, [])  # O padrão é maior que o texto
 
     def test_texto_vazio(self):
         bm = BoyerMoore("ACTG", "ACCA")
         resultado = bm.procurar_padrao("")
-        self.assertEqual(resultado, [])
+        self.assertEqual(resultado, [])  # Texto vazio não contém padrão
 
     def test_padrao_vazio(self):
         bm = BoyerMoore("ACTG", "")
         resultado = bm.procurar_padrao("ACCA")
-        self.assertEqual(resultado, [])
+        self.assertEqual(resultado, [])  # Padrão vazio retorna lista vazia
 
 if __name__ == '__main__':
     unittest.main()
